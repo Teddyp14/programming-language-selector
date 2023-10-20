4925e99bc7e3921fe270dfc5f18951a9ddcb9b54
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2023-10-20 14:49:37 -0700

    Add completed branching logic and updates to the messages displayed to the user.

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,12 @@
 
 function hideResults() {
     document.getElementById("pythonResponse").setAttribute("class", "hidden");
+    document.getElementById("goResponse").setAttribute("class", "hidden");
+    document.getElementById("cResponse").setAttribute("class", "hidden");
     document.getElementById("sqlResponse").setAttribute("class", "hidden");
+    document.getElementById("javascriptResponse").setAttribute("class", "hidden");
 }
+
 
 window.addEventListener("load", function () {
     const form = document.getElementById("languageQuestionnaire");
@@ -27,30 +31,28 @@ function determineLanguage(event) {
     const beverage = document.querySelector("input[name='input5']:checked").value;
     let result;
 
-    console.log(classPreference);
-    console.log(outdoorLove);
-    console.log(emailCheck);
-    console.log(likeGames);
-    console.log(beverage);
-
 
     if (classPreference === "english") {
-        result = "Python! You'll be able write some awesome online blogs with this language!";
+        result = "Python! You can do anything from building a website to taking advantage of machine learning!";
         document.getElementById("pythonResponse").removeAttribute("class");
         document.getElementById("result1").innerText = userName + ", " + "you should learn " + result;
     } else if (classPreference === "math" && (outdoorLove === "1" || outdoorLove === "2") && emailCheck === "constantly") {
-        result = "SQL! No spreadsheet will intimdate you if you know this language!";
-        document.getElementById("sqlResponse").removeAttribute("class");
+        result = "Go! You can work on cutting edge projects like cloud-based applications and AI development!";
+        document.getElementById("goResponse").removeAttribute("class");
         document.getElementById("result2").innerText = userName + ", " + "you should learn " + result;
     } else if (likeGames === "yes" && (outdoorLove === "3" || outdoorLove === "4" || outdoorLove === "5") && beverage === "coffee") {
-        result = "C++! Get going on creating some Skyrim mods!";
+        result = "C++ and make some Skyrim mods!";
+        document.getElementById("cResponse").removeAttribute("class");
+        document.getElementById("result3").innerText = userName + ", " + "you should learn " + result;
     } else if (classPreference === "math" && outdoorLove === "1" && emailCheck === "never" && likeGames === "unknown" && beverage === "energyDrink") {
-        result = "Binary... Godspeed.";
+        result = "SQL! You'll be able to handle big data and wrestle any spreadsheet into submission!";
+        document.getElementById("sqlResponse").removeAttribute("class");
+        document.getElementById("result4").innerText = userName + ", " + "you should learn " + result;
     } else {
         result = "JavaScript! Go look up Epicodus! "
+        document.getElementById("javascriptResponse").removeAttribute("class");
+        document.getElementById("result5").innerText = userName + ", " + "you should learn " + result;
     }
-
-    console.log(result)
 
 
 
