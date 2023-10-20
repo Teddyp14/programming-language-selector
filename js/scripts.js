4eb3ae1cb0685beef0190b8d2e9a5bd5840ead5d
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2023-10-20 11:56:08 -0700

    WIP adjustments to how the results are displayed and CSS design.

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,7 @@ window.addEventListener("load", function () {
 
 function determineLanguage(event) {
     event.preventDefault();
+    const userName = document.querySelector("input#user").value;
     const classPreference = document.querySelector("input[name='input1']:checked").value;
     const outdoorLove = document.querySelector("input[name='input2']:checked").value;
     const emailCheck = document.querySelector("input[name='input3']:checked").value;
@@ -24,19 +25,19 @@ function determineLanguage(event) {
 
 
     if (classPreference === "english") {
-        result = "python";
+        result = "Python! You'll be able write some awesome online blogs with this language!"
     } else if (classPreference === "math" && (outdoorLove === "1" || outdoorLove === "2") && emailCheck === "constantly") {
-        result = "SQL";
+        result = "SQL! No spreadsheet will intimdate you if your know this language!";
     } else if (likeGames === "yes" && (outdoorLove === "3" || outdoorLove === "4" || outdoorLove === "5") && beverage === "coffee") {
-        result = "skyrim coding language";
+        result = "C++! Get going on creating some Skyrim mods!";
     } else if (classPreference === "math" && outdoorLove === "1" && emailCheck === "never" && likeGames === "unknown" && beverage === "energyDrink") {
-        result = "Binary";
+        result = "Binary... Godspeed.";
     } else {
-        result = "javascript"
+        result = "JavaScript! Go look up Epicodus! "
     }
 
     console.log(result)
-    document.getElementById("result").innerText = result;
+    document.getElementById("result").innerText = userName + ", " + "you should learn " + result;
 
 
 }
