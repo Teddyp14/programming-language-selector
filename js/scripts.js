a98044e1a21c224744cbfc7a70175612c6933c35
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2023-10-20 12:43:46 -0700

    WIP to correct a bug preventing correct message from being displayed to the user.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,12 @@
 
 
 //User Interface Logic
+
+function hideResults() {
+    document.getElementById("pythonResponse").setAttribute("class", "hidden");
+    document.getElementById("sqlResponse").setAttribute("class", "hidden");
+}
+
 window.addEventListener("load", function () {
     const form = document.getElementById("languageQuestionnaire");
     form.addEventListener("submit", determineLanguage);
@@ -10,12 +16,16 @@ window.addEventListener("load", function () {
 
 function determineLanguage(event) {
     event.preventDefault();
+
+    hideResults();
+
     const userName = document.querySelector("input#user").value;
     const classPreference = document.querySelector("input[name='input1']:checked").value;
     const outdoorLove = document.querySelector("input[name='input2']:checked").value;
     const emailCheck = document.querySelector("input[name='input3']:checked").value;
     const likeGames = document.querySelector("input[name='input4']:checked").value;
     const beverage = document.querySelector("input[name='input5']:checked").value;
+    let result;
 
     console.log(classPreference);
     console.log(outdoorLove);
@@ -25,9 +35,11 @@ function determineLanguage(event) {
 
 
     if (classPreference === "english") {
-        result = "Python! You'll be able write some awesome online blogs with this language!"
+        result = "Python! You'll be able write some awesome online blogs with this language!";
+        document.getElementById("pythonResponse").removeAttribute("class");
     } else if (classPreference === "math" && (outdoorLove === "1" || outdoorLove === "2") && emailCheck === "constantly") {
-        result = "SQL! No spreadsheet will intimdate you if your know this language!";
+        result = "SQL! No spreadsheet will intimdate you if you know this language!";
+        document.getElementById("sqlResponse").removeAttribute("class");
     } else if (likeGames === "yes" && (outdoorLove === "3" || outdoorLove === "4" || outdoorLove === "5") && beverage === "coffee") {
         result = "C++! Get going on creating some Skyrim mods!";
     } else if (classPreference === "math" && outdoorLove === "1" && emailCheck === "never" && likeGames === "unknown" && beverage === "energyDrink") {
